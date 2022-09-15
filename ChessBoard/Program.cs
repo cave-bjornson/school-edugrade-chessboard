6193bf34d463884064ba87576e2641f5e607638a
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-15 23:09:01 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("◼︎◻︎♛♜♞");
+        }
+    }
+}
