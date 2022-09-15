1893b778061239f06ae4c91e53867ef12c1113b9
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-16 01:15:19 +0200

    Input size completed

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -6,6 +6,24 @@
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("◼︎◻︎♛♜♞");
+            
+            // Interface with the user
+            Console.Write("Enter size of chessboard (1-64): ");
+            uint size = 0;
+            bool acceptedSize = false;
+            while (!acceptedSize)
+            {
+                if (uint.TryParse(Console.ReadLine(), out size))
+                {
+                    acceptedSize = (size >= 1 && size <= 64);
+                    if (acceptedSize)
+                    {
+                        break;
+                    }
+                }
+                Console.Write("Enter a _number_ between 1 and 64: ");
+            }
+            Console.WriteLine($"Chessboard Size: {size}x{size}");
         }
     }
 }
